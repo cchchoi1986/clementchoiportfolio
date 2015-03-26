b393c1a4c7c8d3aae906eb4b52c8ac5dfcdfc156
cchchoi1986 <cchchoi1986@gmail.com>
2015-03-26 22:33:20 +0800

    added batman and google analytics

--- client/app/sandbox/sandbox.controller.js
+++ client/app/sandbox/sandbox.controller.js
@@ -4,4 +4,5 @@ angular.module('clementchoiportfolioApp')
   .controller('SandboxCtrl', ['$scope', '$location', '$state', function ($scope, $location, $state) {
     // $scope.message = 'Hello';
     $scope.placeMark = $state.current.url;
+    
   }]);
